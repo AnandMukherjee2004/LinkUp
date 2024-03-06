0e27471aee696317b046af7b70189605936a8342
AnandMukherjee2004 <anandmukherjee2022@vitbhopal.ac.in>
2024-03-07 01:09:08 +0530

    Loading page top margin

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ function Loading() {
   return (
     // <div className="w-10 h-10 rounded-full border-4 border-pink-200 animate-spin mx-auto"></div>
     <div
-      className=" flex justify-center items-center h-[80vh]"
+      className=" flex justify-center items-center mt-52"
     >
       <span className="loading loading-infinity text-purple-600 h-10 w-16"></span>
     </div>
